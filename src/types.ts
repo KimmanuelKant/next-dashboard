c0d908ea95cca94311db07cde8c9ea66d1f89559
KimmanuelKant <112461883+KimmanuelKant@users.noreply.github.com>
2024-10-04 07:06:20 +0200

    added type for team data api response

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,21 @@
 // src/types.ts
 
+export interface ApiTeam {
+  rank: number;
+  player_name: string;
+  entry_name: string;
+  total: number;
+  event_total: number;
+  // Include any other properties from the API response that you need
+}
+
 export interface Team {
-    rank: number;
-    managerName: string;
-    teamName: string;
-    totalPoints: number;
-    eventTotal: number;
-  }
+  rank: number;
+  managerName: string;
+  teamName: string;
+  totalPoints: number;
+  eventTotal: number;
+}
   
   export interface Manager {
     player_name: string;
