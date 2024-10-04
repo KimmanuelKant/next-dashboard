d2ecef446ff6e551cdc275c54343825f4f4a2461
KimmanuelKant <112461883+KimmanuelKant@users.noreply.github.com>
2024-10-04 06:02:11 +0200

    Refactor dashboard layout component
    - Import ReactNode type from "react" in layout.tsx
    - Add missing semicolon in import statement
    - Update navigation links to use Bootstrap nav-tabs class
    - Remove commented out code for navigation links

--- src/app/dashboard/[leagueId]/layout.tsx
+++ src/app/dashboard/[leagueId]/layout.tsx
@@ -1,12 +1,12 @@
 // src/app/dashboard/[leagueId]/layout.tsx
-import { ReactNode } from "react";
 import Link from "next/link";
-import  "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function DashboardLayout({
   children,
   params,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
   params: { leagueId: string };
 }) {
   const { leagueId } = params;
@@ -15,25 +15,28 @@ export default function DashboardLayout({
     <div>
       <h1>FPL Dashboard for League {leagueId}</h1>
       <nav>
-
-              <ul className="nav">
-        <li className="nav-item">
-          <Link className="nav-link" href={`/dashboard/${leagueId}/team`}>Team Statistics</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href={`/dashboard/${leagueId}/player`} >Player Statistics</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href={`/dashboard/${leagueId}/awards`} >Awards</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href={`/dashboard/${leagueId}/chat`} >Chat</Link>
-        </li> 
-    </ul>
-    {/*     <Link href={`/dashboard/${leagueId}/team`}>Team Statistics</Link>
-        <Link href={`/dashboard/${leagueId}/player`}>Player Statistics</Link>
-        <Link href={`/dashboard/${leagueId}/awards`}>Awards</Link>
-        <Link href={`/dashboard/${leagueId}/chat`}>Chat</Link> */}
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link className="nav-link" href={`/dashboard/${leagueId}/team`}>
+              Team Statistics
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href={`/dashboard/${leagueId}/player`}>
+              Player Statistics
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href={`/dashboard/${leagueId}/awards`}>
+              Awards
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href={`/dashboard/${leagueId}/chat`}>
+              Chat
+            </Link>
+          </li>
+        </ul>
       </nav>
       <div>{children}</div>
     </div>
