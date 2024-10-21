b08a0e2d72e4d1e840ab01d485279e003d41e933
KimmanuelKant <112461883+KimmanuelKant@users.noreply.github.com>
2024-10-21 10:29:55 +0200

    Add League ID instructions to front page accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,6 +56,24 @@ export default function HomePage() {
         </div>
         <Accordion defaultActiveKey="0" flush>
       <Accordion.Item eventKey="0">
+        <Accordion.Header>How to find your League ID</Accordion.Header>
+        <Accordion.Body>
+          <p>
+            Head to the <a href="https://fantasy.premierleague.com/leagues" target="_blank" rel="noreferrer">Fantasy Premier Leagues</a> page and click on your league. The League ID is the number at the end of the URL.
+            <br />
+            For example, the League ID for <a href="https://fantasy.premierleague.com/leagues/2355363/standings/c" target="_blank" rel="noreferrer">MyFPL.net's league</a> is <strong>2355363</strong>.
+          </p>
+          <Image
+            src="/findleagueid.PNG"
+            alt="Screenshot showing where to find the League ID"
+            width={450} // Adjust width as needed
+            height={40} // Adjust height as needed
+            className="img-fluid mt-3"
+          />
+        </Accordion.Body>
+
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
         <Accordion.Header>About MyFPL.net</Accordion.Header>
         <Accordion.Body>
           <p>
