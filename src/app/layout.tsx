ceb4b1c37c61774a0b1d253692a6388b25ee15d9
KimmanuelKant <112461883+KimmanuelKant@users.noreply.github.com>
2024-10-21 09:38:47 +0200

    new titled and expanded description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/global.css";
 import { Analytics } from "@vercel/analytics/react"
 
+
 export const metadata = {
-  title: "FPL Dashboard",
-  description: "Fantasy Premier League Dashboard",
+  title: "MyFPL.net | Fantasy Premier League Minileague Dashboard",
+  description: "Fantasy Premier League Minileague Dashboard. Enter your League ID to access your personalized dashboard with customizable tables, charts, and statistics.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-bs-theme="dark">
       <body>
+        
         {children}
         <Analytics />
         </body>
