22d844741fc9a55d1bd4ace35a046aeb0fd4edc0
KimmanuelKant <112461883+KimmanuelKant@users.noreply.github.com>
2024-12-12 13:23:15 +0100

    added type for league standing api-call

---
+++ src/types/fpl/FplLeagueStandings.ts
@@ -0,0 +1,47 @@
+export interface FplLeagueNewEntries {
+    has_next: boolean;
+    page: number;
+    results: any[];
+  }
+  
+  export interface FplLeagueInfo {
+    id: number;
+    name: string;
+    created: string;
+    closed: boolean;
+    max_entries: number | null;
+    league_type: string;
+    scoring: string;
+    admin_entry: number;
+    start_event: number;
+    code_privacy: string;
+    has_cup: boolean;
+    cup_league: null | number;
+    rank: number | null;
+  }
+  
+  export interface FplLeagueStandingResult {
+    id: number;
+    event_total: number;
+    player_name: string;
+    rank: number;
+    last_rank: number;
+    rank_sort: number;
+    total: number;
+    entry: number;
+    entry_name: string;
+  }
+  
+  export interface FplLeagueStandingsData {
+    has_next: boolean;
+    page: number;
+    results: FplLeagueStandingResult[];
+  }
+  
+  export interface FplLeagueStandings {
+    new_entries: FplLeagueNewEntries;
+    last_updated_data: string;
+    league: FplLeagueInfo;
+    standings: FplLeagueStandingsData;
+  }
+  
